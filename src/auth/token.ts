ddc04ca3d3e8c653b5385401c35ba4e2d40d7d73
vbuser2004 <football@pitney.com>
2023-10-13 19:15:21 -0400

    Auth build

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -1,100 +1,102 @@
-import 'dotenv/config';
+import "dotenv/config";
 import type {
-    AuthenticationOptions,
-    CredentialsResponse,
-} from '../types/Authentication.js';
+  AuthenticationOptions,
+  CredentialsResponse,
+} from "../types/Authentication.js";
 import {
-    AuthenticationOptions_Schema,
-    AuthenticationResponseError_Schema,
-    Credentials_Schema,
-} from '../types/Authentication.js';
+  AuthenticationOptions_Schema,
+  AuthenticationResponseError_Schema,
+  Credentials_Schema,
+} from "../types/Authentication.js";
 
 const fetchToken = async (
-    authOptions: AuthenticationOptions
+  authOptions: AuthenticationOptions
 ): Promise<string> => {
-    const bodyData = {
-        grant_type: 'client_credentials',
-        client_id: authOptions.UAID,
-        client_secret: authOptions.secretKey,
-    };
+  const bodyData = {
+    grant_type: "client_credentials",
+    client_id: authOptions.UAID,
+    client_secret: authOptions.secretKey,
+  };
 
-    const isValidAuthOptions = AuthenticationOptions_Schema.safeParse(bodyData);
+  const isValidAuthOptions = AuthenticationOptions_Schema.safeParse(bodyData);
 
-    if (!isValidAuthOptions.success)
-        return JSON.stringify({ state: 'error', msg: 'Invalid Parameters' });
+  if (!isValidAuthOptions.success)
+    return JSON.stringify({ state: "error", msg: "Invalid Parameters" });
 
-    const response = await fetch(authOptions.authURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'yoyoapi',
-            'yoyoapi-Version': process.env.YOYOAPI_VERSION || '',
-        },
-        body: JSON.stringify(bodyData),
-    });
+  const response = await fetch(authOptions.authURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "yoyoapi",
+      "yoyoapi-Version": process.env.YOYOAPI_VERSION || "",
+    },
+    body: JSON.stringify(bodyData),
+  });
 
-    if (response.ok) {
-        return await response.json();
-    } else {
-        return JSON.stringify({ state: 'error', msg: 'Invalid Parameters' });
-    }
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return JSON.stringify({ state: "error", msg: "Invalid Parameters" });
+  }
 };
 
 const fillInCredentials = async (
-    responsePackage: CredentialsResponse
+  responsePackage: CredentialsResponse
 ): Promise<CredentialsResponse> => {
-    const creds: CredentialsResponse = {
-        success: responsePackage.success,
-        message: responsePackage.message,
-        request_time: Math.floor(new Date().getTime() / 1000),
-        data: responsePackage.data,
-    };
+  const creds: CredentialsResponse = {
+    success: responsePackage.success,
+    message: responsePackage.message,
+    request_time: Math.floor(new Date().getTime() / 1000),
+    data: responsePackage.data,
+  };
 
-    return creds;
+  return creds;
 };
 
-export const auth = async ({
-    UAID,
-    secretKey,
-    authURL = process.env.YOLINK_AUTH_URL!,
+const auth = async ({
+  UAID,
+  secretKey,
+  authURL = process.env.YOLINK_AUTH_URL!,
 }: AuthenticationOptions): Promise<CredentialsResponse> => {
-    const authResponse = await fetchToken({ UAID, secretKey, authURL });
+  const authResponse = await fetchToken({ UAID, secretKey, authURL });
 
-    // Invalid response from server
-    if (!authResponse) {
-        return fillInCredentials({
-            success: false,
-            message: 'Server Error',
-            data: undefined,
-        });
-    }
+  // Invalid response from server
+  if (!authResponse) {
+    return fillInCredentials({
+      success: false,
+      message: "Server Error",
+      data: undefined,
+    });
+  }
 
-    const isError = AuthenticationResponseError_Schema.safeParse(authResponse);
+  const isError = AuthenticationResponseError_Schema.safeParse(authResponse);
 
-    // Error response from server
-    if (isError.success) {
-        // Error from login
-        return fillInCredentials({
-            success: false,
-            message: JSON.stringify(isError.data),
-            data: undefined,
-        });
-    }
+  // Error response from server
+  if (isError.success) {
+    // Error from login
+    return fillInCredentials({
+      success: false,
+      message: JSON.stringify(isError.data),
+      data: undefined,
+    });
+  }
 
-    const isCredentials = Credentials_Schema.safeParse(authResponse);
+  const isCredentials = Credentials_Schema.safeParse(authResponse);
 
-    if (isCredentials.success) {
-        // Valid credentials
-        return fillInCredentials({
-            success: true,
-            message: '',
-            data: isCredentials.data,
-        });
-    } else {
-        return fillInCredentials({
-            success: false,
-            message: 'Configuration Error',
-            data: undefined,
-        });
-    }
+  if (isCredentials.success) {
+    // Valid credentials
+    return fillInCredentials({
+      success: true,
+      message: "",
+      data: isCredentials.data,
+    });
+  } else {
+    return fillInCredentials({
+      success: false,
+      message: "Configuration Error",
+      data: undefined,
+    });
+  }
 };
+
+export default auth;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { CredentialsResponse, AuthenticationOptions_Schema, AuthenticationOptions } from './types/Authentication.js';
+import {
+  CredentialsResponse,
+  AuthenticationOptions_Schema,
+  AuthenticationOptions,
+} from "./types/Authentication.js";
+
+import auth from "./auth/token.js";
 
 // const authoptions = {
 //     UAID: process.env.YOLINK_UAID,
@@ -6,33 +12,42 @@ import { CredentialsResponse, AuthenticationOptions_Schema, AuthenticationOption
 // }
 
 class yoyoApi {
+  // PUBLIC VARIABLES
+  public credentials: CredentialsResponse;
+  public ApiURL: string;
+  public AuthenticationURL: string;
+  private authOptions: AuthenticationOptions;
+  private AuthExpire: number; //Expiration time of the current credentials
 
-    private credentials: CredentialsResponse;
-    private authOptions: AuthenticationOptions;
-    private ApiURL: string;
-    
-    constructor (UAID: string, SecretKey: string, AuthenticationURL?: string, ApiURL?: string) {
+  // CONSTRUCTOR
+  constructor(
+    UAID: string,
+    SecretKey: string,
+    AuthenticationURL: string = process.env.YOSMART_AUTH_URL,
+    ApiURL: string = process.env.YOSMART_API_URL
+  ) {
+    // Validate Authentication Details
+    const isAuthOptions = AuthenticationOptions_Schema.safeParse({
+      UAID,
+      SecretKey,
+      AuthURL: AuthenticationURL,
+    });
 
-        // Validate Authentication Details
-        const isAuthOptions = AuthenticationOptions_Schema.safeParse({
-            UAID,
-            SecretKey,
-            AuthURL: AuthenticationURL
-        })
-
-        // Assign Authentication Options
-        if(isAuthOptions.success) { 
-            this.authOptions = isAuthOptions.data;
-        } else {
-            throw new Error("Invalid Authentication Details")
-        }
-
-        // Assign alternative ApiURL
-        if(ApiURL) {
-            this.ApiURL = ApiURL
-        }
+    // Assign Authentication Options
+    if (isAuthOptions.success) {
+      this.authOptions = isAuthOptions.data;
+    } else {
+      throw new Error("Invalid Authentication Details");
     }
 
+    this.ApiURL = ApiURL;
+    this.AuthenticationURL = AuthenticationURL;
+  }
+
+  // MODULES
+  async ManualAuthentication() {
+    await auth({ ...this.authOptions });
+  }
 }
 
-export default yoyoApi
+export default yoyoApi;
