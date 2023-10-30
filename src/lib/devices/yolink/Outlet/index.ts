bd4e9abe74ce628dccef50211d985b30eb27d9e4
jlpitney <jeff@pitney.com>
2023-10-30 15:55:36 -0400

    Outlet Refactoring

--- src/lib/devices/yolink/Outlet/index.ts
+++ src/lib/devices/yolink/Outlet/index.ts
@@ -1,157 +1,91 @@
-import Device from "../Device/index.js";
-import { sendRequest } from "../../../request/client.js";
-import * as OutletTypes from "../../../../types/yolink/Outlet.js";
-import { ApiError } from "../../../../types/ApiError.js";
+import Device from '../Device/index.js';
+import { sendRequest } from '../../../request/client.js';
+import * as OutletTypes from '../../../../types/yolink/Outlet.js';
+import { ApiError } from '../../../../types/ApiError.js';
 
-import { getDaysOfWeekMask } from "../../../utility/daysofweek.js";
+import { getDaysOfWeekMask } from '../../../utility/daysofweek.js';
 
 class Outlet extends Device {
-  // FUNCTIONS
-  sendOutletRequest = async (
-    method: string,
-    params: object = {},
-    msgid: string = new Date().getTime().toString()
-  ) => {
-    const msgBody = {
-      targetDevice: this.deviceId,
-      token: this.token,
-      method: `Outlet.${method}`,
-      msgid,
-      params,
+    // FUNCTIONS
+    sendOutletRequest = async (
+        method: string,
+        params: object = {},
+        msgid: string = new Date().getTime().toString()
+    ) => {
+        const msgBody = {
+            targetDevice: this.deviceId,
+            token: this.token,
+            method: `Outlet.${method}`,
+            msgid,
+            params,
+        };
+
+        // Ignore 'any' error as schema will be present
+        const isValidBody =
+            // @ts-ignore
+            OutletTypes[`bDDP_Outlet_${method}_Schema`].safeParse(msgBody);
+
+        // Check to make sure body is type safe
+        if (!isValidBody.success) throw new Error('Invalid Request Body');
+
+        // Send request
+        const safeResp = await sendRequest(msgBody);
+
+        // If error send error data
+        if (!safeResp.success) return safeResp.data;
+
+        //@ts-ignore ignore 'any' error as schema will be present
+        const outletState = OutletTypes[
+            `bUDP_Outlet_${method}_Schema`
+        ].safeParse(safeResp.data);
+
+        if (!outletState.success) throw new Error('Invalid Server Response');
+
+        return outletState.data;
     };
 
-    // Ignore 'any' error as schema will be present
-    const isValidBody =
-      // @ts-ignore
-      OutletTypes[`bDDP_Outlet_${method}_Schema`].safeParse(msgBody);
-
-    // Check to make sure body is type safe
-    if (!isValidBody.success) throw new Error("Invalid Request Body");
-
-    // Send request
-    const safeResp = await sendRequest(msgBody);
-
-    // If error send error data
-    if (!safeResp.success) return safeResp.data;
-
-    //@ts-ignore ignore 'any' error as schema will be present
-    const outletState = OutletTypes[`bUDP_Outlet_${method}_Schema`].safeParse(
-      safeResp.data
-    );
-
-    if (!outletState.success) throw new Error("Invalid Server Response");
-
-    return outletState.data;
-  };
-
-  getState = async (
-    msgid?: string
-  ): Promise<OutletTypes.bUDP_Outlet_getState | ApiError> => {
-    return await this.sendOutletRequest("getState", {}, msgid);
-  };
-
-  setState = async (
-    state: string,
-    msgid: string = new Date().getTime().toString()
-  ): Promise<OutletTypes.bUDP_Outlet_setState | ApiError> => {
-    const msgBody = {
-      targetDevice: this.deviceId,
-      token: this.token,
-      method: "Outlet.setState",
-      msgid,
-      params: { state },
+    getState = async (
+        msgid?: string
+    ): Promise<OutletTypes.bUDP_Outlet_getState | ApiError> => {
+        return await this.sendOutletRequest('getState', {}, msgid);
     };
 
-    const isValidBody =
-      OutletTypes.bDDP_Outlet_setState_Schema.safeParse(msgBody);
-
-    // Check to make sure body is type safe
-    if (!isValidBody.success) throw new Error("Invalid Request Body");
-
-    // Send request
-    const safeResp = await sendRequest(msgBody);
-
-    // If error send error data
-    if (!safeResp.success) return safeResp.data;
-
-    // Not an error so Verify it is valid Outlet response
-    const outletState = OutletTypes.bUDP_Outlet_setState_Schema.safeParse(
-      safeResp.data
-    );
-
-    if (!outletState.success) throw new Error("Invalid Server Response");
-
-    return outletState.data;
-  };
-
-  setDelay = async (
-    delayOn: number = 0,
-    delayOff: number = 0,
-    msgid: string = new Date().getTime().toString()
-  ): Promise<OutletTypes.bUDP_Outlet_setDelay | ApiError> => {
-    const msgBody = {
-      targetDevice: this.deviceId,
-      token: this.token,
-      method: "Outlet.setDelay",
-      msgid,
-      params: { delayOn, delayOff },
+    setState = async (
+        state: string,
+        msgid?: string
+    ): Promise<OutletTypes.bUDP_Outlet_setState | ApiError> => {
+        return await this.sendOutletRequest('setState', { state }, msgid);
     };
 
-    const isValidBody =
-      OutletTypes.bDDP_Outlet_setDelay_Schema.safeParse(msgBody);
+    setDelay = async (
+        delayOn: number = 0,
+        delayOff: number = 0,
+        msgid?: string
+    ): Promise<OutletTypes.bUDP_Outlet_setDelay | ApiError> => {
+        return await this.sendOutletRequest(
+            'setDelay',
+            { delayOn, delayOff },
+            msgid
+        );
+    };
 
-    // Check to make sure body is type safe
-    if (!isValidBody.success) throw new Error("Invalid Request Body");
+    getSchedules = async (
+        msgid: string = new Date().getTime().toString()
+    ): Promise<OutletTypes.bUDP_Outlet_getSchedules | ApiError> => {
+        return await this.sendOutletRequest('getSchedules', {}, msgid);
+    };
 
-    const safeResp = await sendRequest(msgBody);
+    // export const setSchedules = async (
+    //     setSchedulesOptions: Outlet.bDDP_Outlet_setSchedules
+    // ): Promise<Outlet.bUDP_Outlet_setSchedules> => {};
 
-    // If error send error data
-    if (!safeResp.success) return safeResp.data;
+    // export const getVersion = async (
+    //     getVersionOptions: Outlet.bDDP_Outlet_getVersion
+    // ): Promise<Outlet.bUDP_Outlet_getVersion> => {};
 
-    // Not an error so Verify it is valid Outlet response
-    const outletState = OutletTypes.bUDP_Outlet_setDelay_Schema.safeParse(
-      safeResp.data
-    );
-
-    if (!outletState.success) throw new Error("Invalid Server Response");
-
-    return outletState.data;
-  };
-
-  getSchedules = async (
-    msgid: string = new Date().getTime().toString()
-  ): Promise<OutletTypes.bUDP_Outlet_getSchedules | ApiError> => {
-    const safeResp = await sendRequest({
-      targetDevice: this.deviceId,
-      token: this.token,
-      method: "Outlet.getSchedules",
-      msgid,
-    });
-
-    // If error send error data
-    if (!safeResp.success) return safeResp.data;
-
-    // Not an error so Verify it is valid Outlet response
-    const outletState = OutletTypes.bUDP_Outlet_getSchedules_Schema.safeParse(
-      safeResp.data
-    );
-
-    if (!outletState.success) throw new Error("Invalid Server Response");
-
-    return outletState.data;
-  };
-
-  // export const setSchedules = async (
-  //     setSchedulesOptions: Outlet.bDDP_Outlet_setSchedules
-  // ): Promise<Outlet.bUDP_Outlet_setSchedules> => {};
-
-  // export const getVersion = async (
-  //     getVersionOptions: Outlet.bDDP_Outlet_getVersion
-  // ): Promise<Outlet.bUDP_Outlet_getVersion> => {};
-
-  // export const startUpgrade = async (
-  //     startUpgradeOptions: Outlet.bDDP_Outlet_startUpgrade
-  // ): Promise<Outlet.bUDP_Outlet_startUpgrade> => {};
+    // export const startUpgrade = async (
+    //     startUpgradeOptions: Outlet.bDDP_Outlet_startUpgrade
+    // ): Promise<Outlet.bUDP_Outlet_startUpgrade> => {};
 }
 
 export default Outlet;
--- src/types/yolink/Outlet.ts
+++ src/types/yolink/Outlet.ts
@@ -1,64 +1,66 @@
-import { z } from "zod";
-import { baseBDDP_Schema, baseBUDP_Schema } from "../BasicDataPacket.js";
-import { LoraInfo } from "../LoraInfo.js";
+import { z } from 'zod';
+import { baseBDDP_Schema, baseBUDP_Schema } from '../BasicDataPacket.js';
+import { LoraInfo } from '../LoraInfo.js';
 
 // BDDP Outlet.getState
+export const bDDP_Outlet_getState_Schema = baseBDDP_Schema;
+
 export type bDDP_Outlet_getState = z.infer<typeof baseBDDP_Schema>;
 
 // BUDP Outlet.getState
 export const bUDP_Outlet_getState_Schema = baseBUDP_Schema.extend({
-  data: z.object({
-    state: z.enum(["open", "closed"]),
-    delay: z.object({
-      ch: z.number(),
-      on: z.number(),
-      off: z.number(),
+    data: z.object({
+        state: z.enum(['open', 'closed']),
+        delay: z.object({
+            ch: z.number(),
+            on: z.number(),
+            off: z.number(),
+        }),
+        power: z.number().optional(),
+        watt: z.number().optional(),
+        version: z.string(),
+        time: z.string(),
+        tz: z.number(),
+        loraInfo: LoraInfo,
     }),
-    power: z.number().optional(),
-    watt: z.number().optional(),
-    version: z.string(),
-    time: z.string(),
-    tz: z.number(),
-    loraInfo: LoraInfo,
-  }),
 });
 
 export type bUDP_Outlet_getState = z.infer<typeof bUDP_Outlet_getState_Schema>;
 
 // BDDP Outlet.setState
 export const bDDP_Outlet_setState_Schema = baseBDDP_Schema.extend({
-  params: z.object({
-    state: z.enum(["open", "close"]),
-  }),
+    params: z.object({
+        state: z.enum(['open', 'close']),
+    }),
 });
 export type bDDP_Outlet_setState = z.infer<typeof bDDP_Outlet_setState_Schema>;
 
 // BUDP Outlet.setState
 export const bUDP_Outlet_setState_Schema = baseBUDP_Schema.extend({
-  data: z.object({
-    state: z.enum(["open", "closed"]),
-  }),
+    data: z.object({
+        state: z.enum(['open', 'closed']),
+    }),
 });
 export type bUDP_Outlet_setState = z.infer<typeof bUDP_Outlet_setState_Schema>;
 
 // BDDP Outlet.setDelay
 export const bDDP_Outlet_setDelay_Schema = baseBDDP_Schema.extend({
-  params: z.object({
-    delayOn: z.number().optional(),
-    delayOff: z.number().optional(),
-  }),
+    params: z.object({
+        delayOn: z.number().optional(),
+        delayOff: z.number().optional(),
+    }),
 });
 
 export type bDDP_Outlet_setDelay = z.infer<typeof bDDP_Outlet_setDelay_Schema>;
 
 // BUDP Outlet.setDelay
 export const bUDP_Outlet_setDelay_Schema = baseBUDP_Schema.extend({
-  data: z.object({
-    delay: z.object({
-      on: z.number(),
-      off: z.number(),
+    data: z.object({
+        delay: z.object({
+            on: z.number(),
+            off: z.number(),
+        }),
     }),
-  }),
 });
 
 export type bUDP_Outlet_setDelay = z.infer<typeof bUDP_Outlet_setDelay_Schema>;
@@ -68,48 +70,48 @@ export type bDDP_Outlet_getSchedules = z.infer<typeof baseBDDP_Schema>;
 
 // BUDP Outlet.getSchedules
 export const bUDP_Outlet_getSchedules_Schema = baseBUDP_Schema.extend({
-  data: z
-    .record(
-      z.number(),
-      z.object({
-        isValid: z.boolean(),
-        index: z.number(),
-        on: z.string().optional(),
-        off: z.string().optional(),
-        week: z.number(),
-      })
-    )
-    .array(),
+    data: z
+        .record(
+            z.number(),
+            z.object({
+                isValid: z.boolean(),
+                index: z.number(),
+                on: z.string().optional(),
+                off: z.string().optional(),
+                week: z.number(),
+            })
+        )
+        .array(),
 });
 
 export type bUDP_Outlet_getSchedules = z.infer<
-  typeof bUDP_Outlet_getSchedules_Schema
+    typeof bUDP_Outlet_getSchedules_Schema
 >;
 
 // BDDP Outlet.setSchedules
 export const bDDP_Outlet_setSchedules_Schema = baseBDDP_Schema.extend({
-  params: z.object({
-    sches: z.record(
-      z.number(),
-      z.object({
-        isValid: z.boolean(),
-        index: z.number(),
-        on: z.string().optional(),
-        off: z.string().optional(),
-        week: z.number(),
-      })
-    ),
-  }),
+    params: z.object({
+        sches: z.record(
+            z.number(),
+            z.object({
+                isValid: z.boolean(),
+                index: z.number(),
+                on: z.string().optional(),
+                off: z.string().optional(),
+                week: z.number(),
+            })
+        ),
+    }),
 });
 
 export type bDDP_Outlet_setSchedules = z.infer<
-  typeof bDDP_Outlet_setSchedules_Schema
+    typeof bDDP_Outlet_setSchedules_Schema
 >;
 
 // BUDP Outlet.setSchedules
 // Note: Same at getSchedules
 export type bUDP_Outlet_setSchedules = z.infer<
-  typeof bUDP_Outlet_getSchedules_Schema
+    typeof bUDP_Outlet_getSchedules_Schema
 >;
 
 // BDDP Outlet.getVersion
@@ -117,34 +119,34 @@ export type bDDP_Outlet_getVersion = z.infer<typeof baseBDDP_Schema>;
 
 // BUDP Outlet.getVersion
 export const bUDP_Outlet_getVersion_Schema = baseBUDP_Schema.extend({
-  data: z.object({
-    version: z.string(),
-    newVersion: z.object({
-      versionId: z.string().optional(),
-      versionCode: z.string().optional(),
-      fileSize: z.string().optional(),
+    data: z.object({
+        version: z.string(),
+        newVersion: z.object({
+            versionId: z.string().optional(),
+            versionCode: z.string().optional(),
+            fileSize: z.string().optional(),
+        }),
     }),
-  }),
 });
 
 // BDDP Outlet.startUpgrade
 export const bDDP_Outlet_startUpgrade_Schema = baseBDDP_Schema.extend({
-  params: z.object({
-    versionId: z.string(),
-  }),
+    params: z.object({
+        versionId: z.string(),
+    }),
 });
 
 export type bDDP_Outlet_startUpgrade = z.infer<
-  typeof bDDP_Outlet_startUpgrade_Schema
+    typeof bDDP_Outlet_startUpgrade_Schema
 >;
 
 // BUDP Outlet.startUpgrade
 export const bUDP_Outlet_startUpgrade_Schema = baseBUDP_Schema.extend({
-  data: z.object({
-    isStarted: z.boolean(),
-  }),
+    data: z.object({
+        isStarted: z.boolean(),
+    }),
 });
 
 export type bUDP_Outlet_startUpgrade = z.infer<
-  typeof bUDP_Outlet_getVersion_Schema
+    typeof bUDP_Outlet_getVersion_Schema
 >;
