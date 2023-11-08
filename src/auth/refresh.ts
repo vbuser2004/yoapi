88b65f8f2c335575dd5577d675b5f2ef67d707f9
vbuser2004 <football@pitney.com>
2023-11-07 21:27:47 -0500

    Removed dotenv

--- src/auth/refresh.ts
+++ src/auth/refresh.ts
@@ -1,86 +1,85 @@
-import 'dotenv/config';
+import yoyoApi from "../index.js";
 import type {
-    CredentialsResponse,
-    RefreshTokenOptions,
-} from '../types/Authentication.js';
+  CredentialsResponse,
+  RefreshTokenOptions,
+} from "../types/Authentication.js";
 import {
-    AuthenticationResponseError_Schema,
-    Credentials_Schema,
-} from '../types/Authentication.js';
+  AuthenticationResponseError_Schema,
+  Credentials_Schema,
+} from "../types/Authentication.js";
 
-import { fillInCredentials } from './utility.js';
+import { fillInCredentials } from "./utility.js";
 
 const refreshToken = async ({
-    UAID,
-    refresh_token,
-    authURL = process.env.YOLINK_AUTH_URL!,
+  UAID,
+  refresh_token,
+  authURL,
 }: RefreshTokenOptions): Promise<string> => {
-    const bodyData = {
-        grant_type: 'refresh_token',
-        client_id: UAID,
-        refresh_token: refresh_token,
-    };
+  const bodyData = {
+    grant_type: "refresh_token",
+    client_id: UAID,
+    refresh_token: refresh_token,
+  };
 
-    const response = await fetch(authURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'yoyoapi',
-            'yoyoapi-Version': process.env.YOYOAPI_VERSION || '',
-        },
-        body: JSON.stringify(bodyData),
-    });
+  const response = await fetch(authURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "yoyoapi",
+      "yoyoapi-Version": "0.7.4",
+    },
+    body: JSON.stringify(bodyData),
+  });
 
-    if (response.ok) {
-        return await response.json();
-    } else {
-        return JSON.stringify({ state: 'error', msg: 'Invalid Parameters' });
-    }
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return JSON.stringify({ state: "error", msg: "Invalid Parameters" });
+  }
 };
 
 const refresh = async (
-    refreshOptions: RefreshTokenOptions
+  refreshOptions: RefreshTokenOptions
 ): Promise<CredentialsResponse> => {
-    const refreshResponse = await refreshToken(refreshOptions);
+  const refreshResponse = await refreshToken(refreshOptions);
 
-    // Invalid response from server
-    if (!refreshResponse) {
-        return fillInCredentials({
-            success: false,
-            message: 'Server Error',
-            data: undefined,
-        });
-    }
+  // Invalid response from server
+  if (!refreshResponse) {
+    return fillInCredentials({
+      success: false,
+      message: "Server Error",
+      data: undefined,
+    });
+  }
 
-    const isError =
-        AuthenticationResponseError_Schema.safeParse(refreshResponse);
+  const isError = AuthenticationResponseError_Schema.safeParse(refreshResponse);
 
-    // Error response from server
-    if (isError.success) {
-        // Error from login
-        return fillInCredentials({
-            success: false,
-            message: 'Authentication Error',
-            data: undefined,
-        });
-    }
+  // Error response from server
+  if (isError.success) {
+    // Error from login
+    return fillInCredentials({
+      success: false,
+      message: "Authentication Error",
+      data: undefined,
+    });
+  }
 
-    const isCredentials = Credentials_Schema.safeParse(refreshResponse);
+  const isCredentials = Credentials_Schema.safeParse(refreshResponse);
 
-    if (isCredentials.success) {
-        // Valid credentials
-        return fillInCredentials({
-            success: true,
-            message: '',
-            data: isCredentials.data,
-        });
-    } else {
-        return fillInCredentials({
-            success: false,
-            message: 'Configuration Error',
-            data: undefined,
-        });
-    }
+  if (isCredentials.success) {
+    // Valid credentials
+    return fillInCredentials({
+      success: true,
+      message: "",
+      data: isCredentials.data,
+    });
+  } else {
+    return fillInCredentials({
+      success: false,
+      message: "Configuration Error",
+      data: undefined,
+    });
+  }
 };
 
 export default refresh;
--- src/auth/token.ts
+++ src/auth/token.ts
@@ -1,100 +1,99 @@
-import 'dotenv/config';
 import type {
-    AuthenticationOptions,
-    CredentialsResponse,
-} from '../types/Authentication.js';
+  AuthenticationOptions,
+  CredentialsResponse,
+} from "../types/Authentication.js";
 import {
-    AuthenticationOptions_Schema,
-    AuthenticationResponseError_Schema,
-    Credentials_Schema,
-} from '../types/Authentication.js';
+  AuthenticationOptions_Schema,
+  AuthenticationResponseError_Schema,
+  Credentials_Schema,
+} from "../types/Authentication.js";
 
-import { fillInCredentials } from './utility.js';
+import { fillInCredentials } from "./utility.js";
 
 const fetchToken = async (
-    authOptions: AuthenticationOptions
+  authOptions: AuthenticationOptions
 ): Promise<string> => {
-    const bodyData = {
-        grant_type: 'client_credentials',
-        client_id: authOptions.UAID,
-        client_secret: authOptions.secretKey,
-    };
+  const bodyData = {
+    grant_type: "client_credentials",
+    client_id: authOptions.UAID,
+    client_secret: authOptions.secretKey,
+  };
 
-    const response = await fetch(authOptions.authURL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'User-Agent': 'yoyoapi',
-            'yoyoapi-Version': process.env.YOYOAPI_VERSION || '',
-        },
-        body: JSON.stringify(bodyData),
-    });
+  const response = await fetch(authOptions.authURL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "yoyoapi",
+      "yoyoapi-Version": "0.7.4",
+    },
+    body: JSON.stringify(bodyData),
+  });
 
-    if (response.ok) {
-        return await response.json();
-    } else {
-        return JSON.stringify({ state: 'error', msg: 'Invalid Parameters' });
-    }
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return JSON.stringify({ state: "error", msg: "Invalid Parameters" });
+  }
 };
 
 const auth = async ({
+  authURL,
+  UAID,
+  secretKey,
+}: AuthenticationOptions): Promise<CredentialsResponse> => {
+  const isAuthOps = AuthenticationOptions_Schema.safeParse({
     authURL,
     UAID,
     secretKey,
-}: AuthenticationOptions): Promise<CredentialsResponse> => {
-    const isAuthOps = AuthenticationOptions_Schema.safeParse({
-        authURL,
-        UAID,
-        secretKey,
+  });
+
+  if (!isAuthOps.success) {
+    return fillInCredentials({
+      success: false,
+      message: "Configuration Error",
+      data: undefined,
     });
+  }
 
-    if (!isAuthOps.success) {
-        return fillInCredentials({
-            success: false,
-            message: 'Configuration Error',
-            data: undefined,
-        });
-    }
+  const authResponse = await fetchToken(isAuthOps.data);
 
-    const authResponse = await fetchToken(isAuthOps.data);
+  // Invalid response from server
+  if (!authResponse) {
+    return fillInCredentials({
+      success: false,
+      message: "Server Error",
+      data: undefined,
+    });
+  }
 
-    // Invalid response from server
-    if (!authResponse) {
-        return fillInCredentials({
-            success: false,
-            message: 'Server Error',
-            data: undefined,
-        });
-    }
+  const isError = AuthenticationResponseError_Schema.safeParse(authResponse);
 
-    const isError = AuthenticationResponseError_Schema.safeParse(authResponse);
+  // Error response from server
+  if (isError.success) {
+    // Error from login
+    return fillInCredentials({
+      success: false,
+      message: JSON.stringify(isError.data),
+      data: undefined,
+    });
+  }
 
-    // Error response from server
-    if (isError.success) {
-        // Error from login
-        return fillInCredentials({
-            success: false,
-            message: JSON.stringify(isError.data),
-            data: undefined,
-        });
-    }
+  const isCredentials = Credentials_Schema.safeParse(authResponse);
 
-    const isCredentials = Credentials_Schema.safeParse(authResponse);
-
-    if (isCredentials.success) {
-        // Valid credentials
-        return fillInCredentials({
-            success: true,
-            message: '',
-            data: isCredentials.data,
-        });
-    } else {
-        return fillInCredentials({
-            success: false,
-            message: 'Configuration Error',
-            data: undefined,
-        });
-    }
+  if (isCredentials.success) {
+    // Valid credentials
+    return fillInCredentials({
+      success: true,
+      message: "",
+      data: isCredentials.data,
+    });
+  } else {
+    return fillInCredentials({
+      success: false,
+      message: "Configuration Error",
+      data: undefined,
+    });
+  }
 };
 
 export default auth;
