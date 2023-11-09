a9e68336cbbc499449c8a1e0cc83ff6aca906faf
vbuser2004 <football@pitney.com>
2023-11-08 21:55:02 -0500

    Added Auth Tests

--- test/test_package/index.js
+++
@@ -1,9 +0,0 @@
-import yoyoapi from 'yoyoapi';
-
-const startApi = () => {
-    const yo = new yoyoapi('', '', '', 'https://www.yahoo.com');
-
-    console.log('Api URL: ' + yo.ApiURL);
-};
-
-startApi();
---
+++ tests/CreateYoyoApi.test.ts
@@ -0,0 +1,23 @@
+import yoyoapi from "../dist/index.js";
+import { ApiError } from "../dist/types/ApiError.js";
+
+test("YoyoApi Class with Invalid Credentials", () => {
+  expect(() => {
+    //@ts-ignore
+    const yo = new yoyoapi();
+  }).toThrow("Invalid Authentication Details");
+});
+
+test("Class with Invalid ApiUrl", () => {
+  expect(() => {
+    const yo = new yoyoapi("test", "test", "https://example.com", "badurl");
+  }).toThrow("Invalid API Url");
+});
+
+test("Invalid Authentication Credentials", () => {
+  expect(async () => {
+    const yo = new yoyoapi("test", "test");
+    const results = await yo.GetDevices();
+    return results;
+  }).toMatchObject<ApiError>;
+});
