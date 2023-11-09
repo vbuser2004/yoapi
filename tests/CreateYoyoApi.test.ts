39dab9af40e85cceee4f65fdac8a61706207bbef
jlpitney <jeff@pitney.com>
2023-11-08 22:10:45 -0500

    Clean Up Create Test

--- tests/CreateYoyoApi.test.ts
+++ tests/CreateYoyoApi.test.ts
@@ -1,23 +1,29 @@
-import yoyoapi from "../dist/index.js";
-import { ApiError } from "../dist/types/ApiError.js";
+import yoyoapi from '../dist/index.js';
+import { ApiError } from '../dist/types/ApiError.js';
+describe('Create yoyoApi Tests', () => {
+    test('YoyoApi Class with Invalid Credentials', () => {
+        expect(() => {
+            //@ts-ignore
+            const yo = new yoyoapi();
+        }).toThrow('Invalid Authentication Details');
+    });
 
-test("YoyoApi Class with Invalid Credentials", () => {
-  expect(() => {
-    //@ts-ignore
-    const yo = new yoyoapi();
-  }).toThrow("Invalid Authentication Details");
-});
+    test('Class with Invalid ApiUrl', () => {
+        expect(() => {
+            const yo = new yoyoapi(
+                'test',
+                'test',
+                'https://example.com',
+                'badurl'
+            );
+        }).toThrow('Invalid API Url');
+    });
 
-test("Class with Invalid ApiUrl", () => {
-  expect(() => {
-    const yo = new yoyoapi("test", "test", "https://example.com", "badurl");
-  }).toThrow("Invalid API Url");
-});
-
-test("Invalid Authentication Credentials", () => {
-  expect(async () => {
-    const yo = new yoyoapi("test", "test");
-    const results = await yo.GetDevices();
-    return results;
-  }).toMatchObject<ApiError>;
+    test('Invalid Authentication Credentials', () => {
+        expect(async () => {
+            const yo = new yoyoapi('test', 'test');
+            const results = await yo.GetDevices();
+            return results;
+        }).toMatchObject<ApiError>;
+    });
 });
