610e80abfccbaf5f64197b01a87509739d15aab8
jlpitney <jeff@pitney.com>
2023-11-10 21:51:31 -0500

    Testing Updates

--- src/lib/utility/createerror.ts
+++ src/lib/utility/createerror.ts
@@ -1,32 +1,33 @@
-import { ApiError } from "../../types/ApiError.js";
+import { ApiError } from '../../types/ApiError.js';
 
 export const generateError = (
-  code: string,
-  msgid: string,
-  method: string
+    code: string,
+    msgid: string,
+    method: string
 ): ApiError => {
-  let desc;
+    let desc;
 
-  switch (code) {
-    case "700101":
-      desc = "Invalid Server Response";
-      break;
-    case "700102":
-      desc = "Invalid Request Body";
-      break;
-    case "700103":
-      desc = "Authentication Error";
-    default:
-      code = "700999";
-      desc = "Unknown yoyoApi Error";
-  }
+    switch (code) {
+        case '700101':
+            desc = 'Invalid Server Response';
+            break;
+        case '700102':
+            desc = 'Invalid Request Body';
+            break;
+        case '700103':
+            desc = 'Authentication Error';
+            break;
+        default:
+            code = '700999';
+            desc = 'Unknown yoyoApi Error';
+    }
 
-  return {
-    code,
-    time: new Date().getTime(),
-    msgid,
-    method,
-    desc,
-    data: {},
-  };
+    return {
+        code,
+        time: new Date().getTime(),
+        msgid,
+        method,
+        desc,
+        data: {},
+    };
 };
--- tests/GenerateError.test.ts
+++ tests/GenerateError.test.ts
@@ -1,27 +1,32 @@
-import { generateError } from "../dist/lib/utility/createerror.js";
-import { ApiError, ApiError_Schema } from "../dist/types/ApiError.js";
+import { generateError } from '../dist/lib/utility/createerror.js';
 
-describe("Test if generate error works for valid error", () => {
-  test("For error 700101", () => {
-    expect(generateError("700101", "noMgsId", "Outlet.getState").code).toBe(
-      "700101"
-    );
-  });
-  test("For non-existant error - should return 700999", () => {
-    expect(generateError("555555", "noMgsId", "Outlet.getState").code).toBe(
-      "700999"
-    );
-  });
-  test("Error object returns matches ApiError", () => {
-    const res = generateError("700102", "noMsgId", "MotionSensor.getState");
-    expect(res).toMatchObject({
-      code: "700102",
-      data: {},
-      desc: "Invalid Request Body",
-      method: "MotionSensor.getState",
-      msgid: "noMsgId",
-      time: res.time,
+describe('Test if generate error works for valid error', () => {
+    // Cycle through Error List
+    const errorList = ['700101', '700102', '700103'];
+    errorList.map((err) => {
+        test(`Test Error # ${err}`, () => {
+            expect(generateError(err, 'noMsgsId', 'Outlet.getState').code).toBe(
+                err
+            );
+        });
     });
-    expect(res).toBeDefined();
-  });
+
+    test('For non-existant error - should return 700999', () => {
+        expect(generateError('555555', 'noMgsId', 'Outlet.getState').code).toBe(
+            '700999'
+        );
+    });
+
+    test('Error object returns matches ApiError', () => {
+        const res = generateError('700102', 'noMsgId', 'MotionSensor.getState');
+        expect(res).toMatchObject({
+            code: '700102',
+            data: {},
+            desc: 'Invalid Request Body',
+            method: 'MotionSensor.getState',
+            msgid: 'noMsgId',
+            time: res.time,
+        });
+        expect(res).toBeDefined();
+    });
 });
