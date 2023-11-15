ff50145bc058aff7debc2aa9185a3e902e78dd3c
vbuser2004 <football@pitney.com>
2023-11-15 07:49:10 -0500

    Completed Rudimentary Tests

--- src/lib/devices/yolink/DoorSensor/index.ts
+++ src/lib/devices/yolink/DoorSensor/index.ts
@@ -1,35 +1,34 @@
-import Device from '../Device/index.js';
-import * as DoorSensorTypes from '../../../../types/yolink/DoorSensor.js';
-import { sendRequest } from '../../../request/client.js';
-import { ApiError } from '../../../../types/ApiError.js';
-import { generateError } from '../../../utility/createerror.js';
+import Device from "../Device/index.js";
+import * as DoorSensorTypes from "../../../../types/yolink/DoorSensor.js";
+import { sendRequest } from "../../../request/client.js";
+import { ApiError } from "../../../../types/ApiError.js";
+import { generateError } from "../../../utility/createerror.js";
 
 class DoorSensor extends Device {
-    // FUNCTIONS
-    getState = async (
-        msgid: string = new Date().getTime().toString()
-    ): Promise<DoorSensorTypes.bUDP_DoorSensor_getState | ApiError> => {
-        const safeResp = await sendRequest({
-            targetDevice: this.deviceId,
-            token: this.token,
-            method: 'DoorSensor.getState',
-            msgid,
-        });
+  // FUNCTIONS
+  getState = async (
+    msgid: string = new Date().getTime().toString()
+  ): Promise<DoorSensorTypes.bUDP_DoorSensor_getState | ApiError> => {
+    const safeResp = await sendRequest({
+      targetDevice: this.deviceId,
+      token: this.token,
+      method: "DoorSensor.getState",
+      msgid,
+    });
 
-        // If error send error data
-        if (!safeResp.success) return safeResp.data;
+    // If error send error data
+    if (!safeResp.success) return safeResp.data;
 
-        // Not an error so Verify it is valid Motion Sensor response
-        const msState =
-            DoorSensorTypes.bUDP_DoorSensor_getState_Schema.safeParse(
-                safeResp.data
-            );
+    // Not an error so Verify it is valid Motion Sensor response
+    const msState = DoorSensorTypes.bUDP_DoorSensor_getState_Schema.safeParse(
+      safeResp.data
+    );
 
-        if (!msState.success)
-            return await generateError('700101', msgid, 'DoorSensor.getState');
+    if (!msState.success)
+      return generateError("700101", msgid, "DoorSensor.getState");
 
-        return msState.data;
-    };
+    return msState.data;
+  };
 }
 
 export default DoorSensor;
--- tests/YoyoApi.test.ts
+++ tests/YoyoApi.test.ts
@@ -1,10 +1,22 @@
 import yo from "./SetUpYoyoApi";
 
+// Outlet
 const targetOutlet = process.env.OUTLET_TARGET_DEV;
 const targetToken = process.env.OUTLET_TOKEN;
 const bogusOutlet = process.env.OUTLET_TARGET_DEV_FAIL;
 const bogusToken = process.env.OUTLET_TOKEN_FAIL;
+// Door Sensor
+const targetDoorSensor = process.env.DOOR_SENSOR_TARGET_DEV;
+const targetDSToken = process.env.DOOR_SENSOR_TOKEN;
+const bogusDoorSensor = process.env.DOOR_SENSOR_TARGET_DEV_FAIL;
+const bogusDSToken = process.env.DOOR_SENSOR_TOKEN_FAIL;
+// Motion Sensor
+const targetMotionSensor = process.env.MOTION_TARGET_DEV;
+const targetMSToken = process.env.MOTION_TOKEN;
+const bogusMotionSensor = process.env.MOTION_TARGET_DEV_FAIL;
+const bogusMSToken = process.env.MOTION_TOKEN_FAIL;
 
+// Outlet Class
 const goodOutlet = yo.Outlet({
   type: "Outlet",
   deviceId: targetOutlet!,
@@ -16,10 +28,37 @@ const bogusOl = yo.Outlet({
   token: bogusToken!,
 });
 
+// Door Sensor Class
+const goodDoorSensor = yo.DoorSensor({
+  type: "DoorSensor",
+  deviceId: targetDoorSensor!,
+  token: targetDSToken!,
+});
+
+const bogusDS = yo.DoorSensor({
+  type: "DoorSensor",
+  deviceId: bogusDoorSensor!,
+  token: bogusDSToken!,
+});
+
+// Motion Sensor Class
+const goodMotionSensor = yo.MotionSensor({
+  type: "MotionSensor",
+  deviceId: targetMotionSensor!,
+  token: targetMSToken!,
+});
+
+const bogusMS = yo.MotionSensor({
+  type: "MotionSensor",
+  deviceId: bogusMotionSensor!,
+  token: bogusMSToken!,
+});
+
 describe("Home Tests", () => {
   test("Get All Devices", async () => {
     const result = await yo.Home().getDeviceList();
     expect(result).toBeDefined();
+    expect(result.code).toBe("000000");
   });
 
   test("Get All Devices with Specified Message ID", async () => {
@@ -37,6 +76,7 @@ describe("Outlet Tests", () => {
   test("Get Outlet State", async () => {
     const result = await goodOutlet.getState();
     expect(result).toBeDefined();
+    expect(result.code).toBe("000000");
   });
 
   test("Verify Model Name", () => {
@@ -47,6 +87,42 @@ describe("Outlet Tests", () => {
   test("Expect Get Schedules Code to be 000000", async () => {
     const result = await goodOutlet.getSchedules();
     expect(result.code).toBe("000000");
+  });
+
+  test("Expect an Error in Code", async () => {
+    const result = await bogusOl.getState();
+    expect(result.code).toBe("000103");
+  });
+});
+
+describe("Door Sensor Tests", () => {
+  test("Get Door State", async () => {
+    const result = await goodDoorSensor.getState();
+    expect(result).toBeDefined();
+    expect(result.code).toBe("000000");
+  });
+
+  test("Verify Model Name", () => {
+    const result = goodDoorSensor.type;
+    expect(result).toBe("DoorSensor");
+  });
+
+  test("Expect an Error in Code", async () => {
+    const result = await bogusOl.getState();
+    expect(result.code).toBe("000103");
+  });
+});
+
+describe("Motion Sensor Tests", () => {
+  test("Get Motion Sensor State", async () => {
+    const result = await goodMotionSensor.getState();
+    expect(result).toBeDefined();
+    expect(result.code).toBe("000000");
+  });
+
+  test("Verify Model Name", () => {
+    const result = goodMotionSensor.type;
+    expect(result).toBe("MotionSensor");
   });
 
   test("Expect an Error in Code", async () => {
